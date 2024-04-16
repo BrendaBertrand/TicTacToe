c85eb9f32e548c6b87d06197719a76e0d8988609
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-16 22:06:24 +0200

    Creation of a separate file for logic methods

---
+++ TicTacToe/LogicMethods.cs
@@ -0,0 +1,206 @@
+namespace TicTacToe;
+
+public class LogicMethods
+{
+    public static void GridCreation(string[,] grid)
+    {
+        int l = 1;
+        for (int i = 0; i < Constants.GRID_SIZE; i++)
+        {
+            for (int j = 0; j < Constants.GRID_SIZE; j++)
+            {
+                grid[i, j] = (l.ToString());
+                l++;
+            }
+        }
+    }
+
+    public static void GetFinalGrid(string[,] grid, int turn)
+    {
+        int countReplacement = 0;
+        int i = 1;
+        do
+        {
+            if (ReplaceValue(grid, " ", i.ToString()))
+            {
+                countReplacement++;
+            }
+
+            i++;
+        } while (i <= Constants.GRID_SIZE * Constants.GRID_SIZE &&
+                 countReplacement < (Constants.GRID_SIZE * Constants.GRID_SIZE - turn));
+    }
+
+    public static bool CheckUserChoice(string[,] grid, string userInput)
+    {
+        for (int i = 0; i < Constants.GRID_SIZE; i++)
+        {
+            for (int j = 0; j < Constants.GRID_SIZE; j++)
+            {
+                if (userInput == grid[i, j] && userInput != Constants.USER_MARK && userInput != Constants.COMPUTER_MARK)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public static bool ReplaceValue(string[,] grid, string mark, string choice)
+    {
+        bool isReplaced = false;
+        for (int i = 0; i < Constants.GRID_SIZE; i++)
+        {
+            for (int j = 0; j < Constants.GRID_SIZE; j++)
+            {
+                if (grid[i, j] == choice)
+                {
+                    grid[i, j] = mark;
+                    isReplaced = true;
+                    return isReplaced;
+                }
+            }
+        }
+
+        return isReplaced;
+    }
+
+
+    //Check line
+    public static bool CheckLine(string[,] array, int line)
+    {
+        int equal = 0;
+        for (int i = 0; i < Constants.GRID_SIZE - 1; i++)
+        {
+            if (array[line, i] == array[line, i + 1])
+            {
+                equal++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return equal == Constants.GRID_SIZE - 1;
+    }
+
+    //Check column
+    public static bool CheckColumn(string[,] array, int col)
+    {
+        int equal = 0;
+        for (int i = 0; i < Constants.GRID_SIZE - 1; i++)
+        {
+            if (array[i, col] == array[i + 1, col])
+            {
+                equal++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return equal == Constants.GRID_SIZE - 1;
+    }
+
+    //Check diagonal
+    public static bool CheckMainDiagonal(string[,] array)
+    {
+        int equal = 0;
+        for (int i = 0; i < Constants.GRID_SIZE - 1; i++)
+        {
+            if (array[i, i] == array[i + 1, i + 1])
+            {
+                equal++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return equal == Constants.GRID_SIZE - 1;
+    }
+
+    //Check counter diagonal
+    public static bool CheckCounterDiagonal(string[,] array)
+    {
+        int equal = 0;
+        for (int i = 0; i < Constants.GRID_SIZE - 1; i++)
+        {
+            if (array[i, (Constants.GRID_SIZE - 1) - i] == array[i + 1, (Constants.GRID_SIZE - 1) - (i + 1)])
+            {
+                equal++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return equal == Constants.GRID_SIZE - 1;
+    }
+
+    public static string GlobalCheck(string[,] grid)
+    {
+        for (int i = 0; i < Constants.GRID_SIZE; i++)
+        {
+            if (CheckLine(grid, i))
+            {
+                return grid[i, 0];
+            }
+
+            if (CheckColumn(grid, i))
+            {
+                return grid[0, i];
+            }
+        }
+
+        if (CheckMainDiagonal(grid))
+        {
+            return grid[0, 0];
+        }
+
+        if (CheckCounterDiagonal(grid))
+        {
+            return grid[Constants.GRID_SIZE - 1, 0];
+        }
+
+        return Constants.WRONG_CHOICE;
+    }
+
+
+    public static string FindBestMove(string[,] grid, string mark)
+    {
+        string[,] testGrid = (string[,])grid.Clone();
+        string storedValue = "";
+        for (int i = 0; i < Constants.GRID_SIZE; i++)
+        {
+            for (int j = 0; j < Constants.GRID_SIZE; j++)
+            {
+                if (testGrid[i, j] != Constants.USER_MARK && testGrid[i, j] != Constants.COMPUTER_MARK)
+                {
+                    storedValue = testGrid[i, j];
+                    ReplaceValue(testGrid, mark, storedValue);
+
+                    if (GlobalCheck(testGrid) == mark)
+                    {
+                        return storedValue;
+                    }
+
+                    testGrid[i, j] = storedValue;
+                }
+            }
+        }
+
+        if (mark == Constants.USER_MARK)
+        {
+            return storedValue;
+        }
+
+        return Constants.WRONG_CHOICE;
+    }
+}
--- TicTacToe/UIMethods.cs
+++ TicTacToe/UIMethods.cs
@@ -2,49 +2,49 @@ namespace TicTacToe;
 
 public class UIMethods
 {
-
     public static void DisplayWelcomeMessage(string message)
     {
         Console.WriteLine($"\n{message}\n");
     }
-    public static void DisplayGrid(string[,] grid, int gridSize, string userMark, string computerMark)
+
+    public static void DisplayGrid(string[,] grid)
     {
         {
-            for (int i = 0; i < gridSize; i++)
+            for (int i = 0; i < Constants.GRID_SIZE; i++)
             {
-                for (int j = 0; j < gridSize; j++)
+                for (int j = 0; j < Constants.GRID_SIZE; j++)
                 {
                     Console.Write($"{grid[i, j]} ");
-                    if (j == gridSize - 1)
+                    if (j == Constants.GRID_SIZE - 1)
                     {
                         Console.Write("\n");
                     }
                 }
             }
         }
-        Console.WriteLine($"\nYour symbol is : {userMark}\nComputer symbol is : {computerMark}\n");
+        Console.WriteLine(
+            $"\nYour symbol is : {Constants.USER_MARK}\nComputer symbol is : {Constants.COMPUTER_MARK}\n");
     }
 
-    public static string GetUserChoice(string[,] grid, string wrongChoice)
+    public static string GetUserChoice(string[,] grid)
     {
         Console.WriteLine("Please enter the number of the position on which you want to play : ");
         string userInput = Console.ReadLine();
 
-        if (!Program.CheckUserChoice(grid, userInput))
+        if (!LogicMethods.CheckUserChoice(grid, userInput))
         {
             Console.Clear();
             Console.WriteLine($"{userInput} is not an authorized value.\n");
-            return wrongChoice;
+            return Constants.WRONG_CHOICE;
         }
 
         return userInput;
     }
 
-    public static void EndGameMessage(string[,] grid, string message, int gridSize, string computerMark,
-        string userMark)
+    public static void EndGameMessage(string[,] grid, string message)
     {
         Console.Clear();
-        DisplayGrid(grid, gridSize, userMark,computerMark);
+        DisplayGrid(grid);
         Console.WriteLine($"\n{message}");
     }
 
